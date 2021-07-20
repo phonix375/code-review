a342fa6f559166fe8997c7c8a64eb3b83dcdb310
Alexy Kotliar <kotliar.alex@gmail.com>
2021-07-19 20:27:19 -0400

    starter code

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,8 @@
+const { User, Thought } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+}
+
+module.exports = resolvers;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,8 @@
+//import the gql tagged template function
+const { gql } = require('apollo-server-express');
+
+//create the typeDefs
+const typeDefs = gql``;
+
+// export ther typeDefs
+module.exports = typeDefs;
