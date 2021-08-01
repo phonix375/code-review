e7565b2e190375502d9884c3b366f0add66fe65f
Nicholas Hoban <nickslaptop@Nicholass-MacBook-Pro.local>
2021-08-01 14:48:27 -0400

    fixed a bunch of styling and am working on the chat function and intend to impliment it in the personal page

---
+++ client/src/components/peer2peer/peer2peer.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/react-hooks';
+import { ADD_COMMENT } from '../../utils/mutations';
+import "./style.css"
+
+function Peer2peer() {
+
+
+    return (
+
+        
+        <div>
+            
+        </div>
+    )
+}
+
+export default Peer2peer;
